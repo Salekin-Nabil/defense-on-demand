55cc67e1b96ba3fc6c33ef7bfeb1e278772ccbd9
Salekin-Nabil <salekinnabil@gmail.com>
2022-04-17 19:32:57 +0600

    review update and rating

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -9,15 +9,15 @@ const Review = ({review}) => {
     return (
         <div className=''>
             <div className="flex justify-center my-[5vw]">
-                <div className="rounded-3xl shadow-2xl shadow-white bg-[#beafa7] max-w-sm p-5 h-[450px] hover:bg-indigo-50 mx-2">
+                <div className="rounded-3xl shadow-2xl shadow-white bg-[#beafa7] max-w-sm p-5 h-[500px] hover:bg-indigo-50 mx-2">
                     <a className='flex justify-center' href="#!">
                     <img className="rounded-full border-[10px] border-white" src={image} alt=""/>
                     </a>
                     <h4 className='text-[20px] font-bold mt-[40px]'>{name}</h4>
                     <Rating className='mt-[2vw]'
                         initialRating={rating}
-                        emptySymbol={<FontAwesomeIcon style={{color: 'gray'}} icon={faStar} />}
-                        fullSymbol={<FontAwesomeIcon style={{color: 'goldenrod'}} icon={faStar} />}
+                        emptySymbol={<FontAwesomeIcon className='mr-[15px] text-xl' style={{color: 'gray'}} icon={faStar} />}
+                        fullSymbol={<FontAwesomeIcon className='mr-[15px] text-xl text-yellow-400 hover:text-yellow-800' style={{color: ''}} icon={faStar} />}
                         readonly
                     ></Rating>
                     <p className='text-[14px] mt-[40px] font-medium'>{comment}</p>
