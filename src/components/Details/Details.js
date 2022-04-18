c29be6ac740a3c3b095be6ec116f1eaccb02894d
Salekin-Nabil <salekinnabil@gmail.com>
2022-04-18 22:52:52 +0600

    Checkout Form using firebase hook form

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,14 +1,130 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 import './Details.css';
 
 const Details = () => {
     let params = useParams();
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
 
     return (
-        <div>
-            <h1 className='text-5xl font-bold text-white text-center mt-[1vw]'>Service Details: ({params.serviceId})</h1>
+        <div className='mb-10'>
+            <h1 className='text-5xl font-bold text-white text-center mt-[1vw] mb-28'>Service Details: ({params.serviceId})</h1>
+            <div className='flex justify-center'>
+                <div className="block p-6 rounded-xl shadow-lg shadow-white hover:shadow-xl hover:shadow-white bg-[#beafa7] max-w-md mx-4 md:mx-0">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <p className='text-2xl font-semibold mb-4 text-white'>Schedule Appointment</p>
+                        <div className='flex justify-center'>
+                        <div className="form-group mb-6 w-full mr-2">
+                            <input type="text" className="form-control block
+                                w-full
+                                px-3
+                                py-1.5
+                                text-base
+                                font-normal
+                                text-gray-700
+                                bg-white bg-clip-padding
+                                border-4 border-solid border-yellow-600
+                                shadow-lg shadow-white hover:shadow-xl hover:shadow-white
+                                rounded-xl
+                                transition
+                                ease-in-out
+                                m-0
+                                focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none" id="exampleInput7"
+                                placeholder="Name"/>
+                            </div>
+                            <div className="form-group mb-6 w-full">
+                            <input type="email" className="form-control block
+                                w-full
+                                px-3
+                                py-1.5
+                                text-base
+                                font-normal
+                                text-gray-700
+                                bg-white bg-clip-padding
+                                border-4 border-solid border-yellow-600
+                                shadow-lg shadow-white hover:shadow-xl hover:shadow-white
+                                rounded-xl
+                                transition
+                                ease-in-out
+                                m-0
+                                focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none" id="exampleInput8"
+                                placeholder="Email address"/>
+                            </div>
+                        </div>
+                        <div className="form-group mb-6 w-full">
+                            <input type="date" className="form-control block
+                                w-full
+                                px-3
+                                py-1.5
+                                text-base
+                                font-normal
+                                text-gray-700
+                                bg-white bg-clip-padding
+                                border-4 border-solid border-yellow-600
+                                shadow-lg shadow-white hover:shadow-xl hover:shadow-white
+                                rounded-xl
+                                transition
+                                ease-in-out
+                                m-0
+                                focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none" id="exampleInput9"
+                                placeholder="Email address"/>
+                            </div>
+                        <div className="form-group mb-6">
+                        <textarea
+                        className="
+                            form-control
+                            block
+                            w-full
+                            px-3
+                            py-1.5
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border-4 border-solid border-yellow-600
+                            shadow-lg shadow-white hover:shadow-xl hover:shadow-white
+                            rounded-xl
+                            transition
+                            ease-in-out
+                            m-0
+                            focus:text-gray-700 focus:bg-white focus:border-yellow-600 focus:outline-none
+                        "
+                        id="exampleFormControlTextarea13"
+                        rows="3"
+                        placeholder="Your Case Description"
+                        ></textarea>
+                        </div>
+                        <div className="form-group form-check text-center mb-6">
+                        <input type="checkbox"
+                            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                            id="exampleCheck87" checked/>
+                        <label className="form-check-label inline-block text-white" for="exampleCheck87">Send me a copy of this message</label>
+                        </div>
+                        <button type="submit" className="
+                        w-full
+                        px-6
+                        py-2.5
+                        bg-yellow-600
+                        text-white
+                        font-medium
+                        text-xs
+                        leading-tight
+                        uppercase
+                        rounded-xl
+                        hover:bg-yellow-700
+                        focus:bg-yellow-700 focus:shadow-lg focus:outline-none focus:ring-0
+                        active:bg-yellow-800 active:shadow-lg
+                        shadow-lg shadow-white hover:shadow-xl hover:shadow-white
+                        transition
+                        duration-150
+                        ease-in-out">Send</button>
+                    </form>
+                </div>                
+            </div>
         </div>
+        
     );
 };
 
