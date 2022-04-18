01797a4939611e72c26b33363486dd4de13700fc
Salekin-Nabil <salekinnabil@gmail.com>
2022-04-18 20:36:22 +0600

    Facebook login

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -18,24 +18,24 @@ const RequireAuth = ({ children }) => {
         return <Navigate to="/Login" state={{ from: location }} replace />;
     }
 
-    console.log(user);
+    // console.log(user);
 
-    if (!user.emailVerified) {
-        return <div className='text-center mt-5'>
-            <h3 className='text-orange-500 text-3xl my-5'>Your Email is not verified!!</h3>
-            <h5 className='text-purple-500 text-3xl my-5'> Please Verify your email address</h5>
-            <button
-            className='bg-[#beafa7] my-5 rounded-lg py-3 px-7'
-                onClick={async () => {
-                    await sendEmailVerification();
-                    toast('Sent email');
-                }}
-            >
-                Send Verification Email Again
-            </button>
-            <ToastContainer></ToastContainer>
-        </div>
-    }
+    // if (!user.emailVerified) {
+    //     return <div className='text-center mt-5'>
+    //         <h3 className='text-orange-500 text-3xl my-5'>Your Email is not verified!!</h3>
+    //         <h5 className='text-purple-500 text-3xl my-5'> Please Verify your email address</h5>
+    //         <button
+    //         className='bg-[#beafa7] my-5 rounded-lg py-3 px-7'
+    //             onClick={async () => {
+    //                 await sendEmailVerification();
+    //                 toast('Sent email');
+    //             }}
+    //         >
+    //             Send Verification Email Again
+    //         </button>
+    //         <ToastContainer></ToastContainer>
+    //     </div>
+    // }
 
     return children;
 };
