adcda33c59f1ed93f2d7663159fff270bb46259b
Salekin-Nabil <salekinnabil@gmail.com>
2022-04-18 11:10:51 +0600

    secret environment variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANNrT8yYNfWz3TDb8jSMIyp82go61WxV0",
-  authDomain: "defense-on-demand.firebaseapp.com",
-  projectId: "defense-on-demand",
-  storageBucket: "defense-on-demand.appspot.com",
-  messagingSenderId: "828941848104",
-  appId: "1:828941848104:web:64742c590351c13f2d2f1d"
+  apiKey: process.env.REACT_APP_apiKey ,
+  authDomain: process.env.REACT_APP_authDomain ,
+  projectId: process.env.REACT_APP_projectId ,
+  storageBucket: process.env.REACT_APP_storageBucket ,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId ,
+  appId: process.env.REACT_APP_appId 
 };
 
 // Initialize Firebase
